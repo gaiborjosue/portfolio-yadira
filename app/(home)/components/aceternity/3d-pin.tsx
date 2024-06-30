d5d135b70b1e295ddf99a77ba9d20dc97d4d8b7a
gaiborjosue <gaiborjimenezjosue@gmail.com>
2024-06-30 19:48:02 -0400

    fix(publications): Testing pin style

--- app/(home)/components/aceternity/3d-pin.tsx
+++ app/(home)/components/aceternity/3d-pin.tsx
@@ -44,7 +44,7 @@ export const PinContainer = ({
           perspective: "1000px",
           transform: "rotateX(70deg) translateZ(0deg)",
         }}
-        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
+        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-y-1/2"
       >
         <div
           style={{
