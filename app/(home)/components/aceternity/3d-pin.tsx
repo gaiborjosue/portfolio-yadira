337b766eb895878c6a4155d216762e29cc0c9deb
gaiborjosue <gaiborjimenezjosue@gmail.com>
2024-06-30 19:56:49 -0400

    refactor(pin): Simplify 3D pin component styles

--- app/(home)/components/aceternity/3d-pin.tsx
+++ app/(home)/components/aceternity/3d-pin.tsx
@@ -44,13 +44,13 @@ export const PinContainer = ({
           perspective: "1000px",
           transform: "rotateX(70deg) translateZ(0deg)",
         }}
-        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
+        className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-y-1/2"
       >
         <div
           style={{
             transform: transform,
           }}
-          className="absolute left-1/2 p-4 top-1/2  flex  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+          className="absolute left-1/2 p-4 top-1/2  flex rounded-2xl shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700"
         >
           <div className={cn(" relative z-50 ", className)}>{children}</div>
         </div>
