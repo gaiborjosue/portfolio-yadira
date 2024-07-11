3f5a6d9cfbf64d1ff46d32a80e694eda4ac0bfa8
gaiborjosue <gaiborjimenezjosue@gmail.com>
2024-07-10 21:47:12 -0400

    refactor(SparkSection): Improve image lazy loading and fix styles

--- app/(home)/components/SparkSection.tsx
+++ app/(home)/components/SparkSection.tsx
@@ -1,26 +1,33 @@
-"use client";
-import React from "react";
+"use client"
+
+import { Sparkles } from "./lunar/Sparkles"
 import { PinContainer } from "./aceternity/3d-pin";
+import SparklesText from "./magicui/sparkle-text";
 
 export function AnimatedPinDemo() {
   return (
-    <div className="h-[40rem] w-full flex items-center justify-center ">
-      <PinContainer
-        title="/ui.aceternity.com"
-        href="https://twitter.com/mannupaaji"
-      >
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            Aceternity UI
-          </h3>
-          <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-500 ">
-              Customizable Tailwind CSS and Framer Motion Components.
-            </span>
+    <>
+      <div className="h-[40rem] w-full items-center justify-center">
+        <PinContainer
+          title="/arxiv.org"
+          href="https://arxiv.org/abs/2006.11276"
+        >
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+            <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+              An Improved Spin-Down Rate for the Proposed White-Dwarf Pulsar AR Scorpii
+            </h3>
+            <div className="text-base !m-0 !p-0 font-normal">
+              <span className="text-slate-500 ">
+                Read more about my most recent publication. 📄
+              </span>
+            </div>
+            <div className="flex flex-1 w-full rounded-lg mt-4 bg-cover bg-center" style={{ backgroundImage: "url('/images/paper_1.webp')" }} />
           </div>
-          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
-        </div>
-      </PinContainer>
-    </div>
-  );
+        </PinContainer>
+
+      </div>
+
+      
+    </>
+  )
 }
